4a216c2dbf3336a9fdbf7c159d321cd7c3b153df
BrunoValcarenghi <valcarenghimachado55@gmail.com>
2026-08-19 14:44:44 -0300

    suporte a um  parametro

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import sys
+
 def main():
-    print("Olá mundo")
+    if len(sys.argv) > 1:
+        print("Olá, ", sys.argv[1])
+    else :
+        print("olá mundo")
 
 if __name__ == "__main__":
     main()
